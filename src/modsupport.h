945716669e5dc99101da066aa547de8b7b73de9a
Furkan Onder <furkanonder@protonmail.com>
2024-10-20 17:18:32 +0300

    Add function declarations to modsupport.h

--- src/modsupport.h
+++ src/modsupport.h
@@ -2,4 +2,9 @@
 
 extern object *initmodule PROTO((char *, struct methodlist *));
 extern int getintarg(object *v, int *a);
+extern int getnoarg(object *v);
+extern int getstrarg(object *v, object **a);
+extern int getstrstrarg(object *v, object **a, object **b);
+extern int getstrintarg(object *v, object **a, int *b);
+extern int getlonglongargs(object *v, long *a, long *b);
 extern void fatal(char *msg);
