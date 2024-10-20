eeffe434eb18e6917aa508ea4367417e023e73d5
Furkan Onder <furkanonder@protonmail.com>
2024-10-20 17:56:03 +0300

    Add setpythonpath and setpythonargv declarations to sysmodule.h

--- src/sysmodule.h
+++ src/sysmodule.h
@@ -4,3 +4,5 @@ object *sysget PROTO((char *));
 int sysset PROTO((char *, object *));
 FILE *sysgetfile PROTO((char *, FILE *));
 void initsys PROTO((void));
+extern void setpythonpath(char *path);
+extern void setpythonargv(int argc, char **argv);
