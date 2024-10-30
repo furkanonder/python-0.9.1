9c3f5ab3dd171545cfbfd84960289a3f0a755215
Furkan Onder <furkanonder@protonmail.com>
2024-10-31 01:00:15 +0300

    Convert K&R style function declarations to ANSI C style in bltinmodule.c

--- src/bltinmodule.c
+++ src/bltinmodule.c
@@ -15,9 +15,7 @@
 #include "fgetsintr.h"
 
 static object *
-builtin_abs(self, v)
-	object *self;
-	object *v;
+builtin_abs(object *self, object *v)
 {
 	/* XXX This should be a method in the as_number struct in the type */
 	if (v == NULL) {
@@ -25,14 +23,16 @@ builtin_abs(self, v)
 	}
 	else if (is_intobject(v)) {
 		long x = getintvalue(v);
-		if (x < 0)
+		if (x < 0) {
 			x = -x;
+        }
 		return newintobject(x);
 	}
 	else if (is_floatobject(v)) {
 		double x = getfloatvalue(v);
-		if (x < 0)
+		if (x < 0) {
 			x = -x;
+        }
 		return newfloatobject(x);
 	}
 	err_setstr(TypeError, "abs() argument must be float or int");
@@ -40,12 +40,11 @@ builtin_abs(self, v)
 }
 
 static object *
-builtin_chr(self, v)
-	object *self;
-	object *v;
+builtin_chr(object *self, object *v)
 {
 	long x;
 	char s[1];
+
 	if (v == NULL || !is_intobject(v)) {
 		err_setstr(TypeError, "chr() must have int argument");
 		return NULL;
@@ -60,18 +59,16 @@ builtin_chr(self, v)
 }
 
 static object *
-builtin_dir(self, v)
-	object *self;
-	object *v;
+builtin_dir(object *self, object *v)
 {
 	object *d;
+
 	if (v == NULL) {
 		d = getlocals();
 	}
 	else {
 		if (!is_moduleobject(v)) {
-			err_setstr(TypeError,
-				"dir() argument, must be module or absent");
+			err_setstr(TypeError, "dir() argument, must be module or absent");
 			return NULL;
 		}
 		d = getmoduledict(v);
@@ -85,12 +82,11 @@ builtin_dir(self, v)
 }
 
 static object *
-builtin_divmod(self, v)
-	object *self;
-	object *v;
+builtin_divmod(object *self, object *v)
 {
 	object *x, *y;
 	long xi, yi, xdivy, xmody;
+
 	if (v == NULL || !is_tupleobject(v) || gettuplesize(v) != 2) {
 		err_setstr(TypeError, "divmod() requires 2 int arguments");
 		return NULL;
@@ -113,7 +109,7 @@ builtin_divmod(self, v)
 	else {
 		xdivy = xi / yi;
 	}
-	xmody = xi - xdivy*yi;
+	xmody = xi - xdivy * yi;
 	if (xmody < 0 && yi > 0 || xmody > 0 && yi < 0) {
 		xmody += yi;
 		xdivy -= 1;
@@ -121,9 +117,9 @@ builtin_divmod(self, v)
 	v = newtupleobject(2);
 	x = newintobject(xdivy);
 	y = newintobject(xmody);
-	if (v == NULL || x == NULL || y == NULL ||
-		settupleitem(v, 0, x) != 0 ||
-		settupleitem(v, 1, y) != 0) {
+	if (v == NULL || x == NULL || y == NULL || settupleitem(v, 0, x) != 0
+        || settupleitem(v, 1, y) != 0)
+    {
 		XDECREF(v);
 		XDECREF(x);
 		XDECREF(y);
@@ -133,53 +129,48 @@ builtin_divmod(self, v)
 }
 
 static object *
-exec_eval(v, start)
-	object *v;
-	int start;
+exec_eval(object *v, int start)
 {
 	object *str = NULL, *globals = NULL, *locals = NULL;
 	int n;
+
 	if (v != NULL) {
-		if (is_stringobject(v))
+		if (is_stringobject(v)) {
 			str = v;
-		else if (is_tupleobject(v) &&
-				((n = gettuplesize(v)) == 2 || n == 3)) {
+        }
+		else if (is_tupleobject(v) && ((n = gettuplesize(v)) == 2 || n == 3)) {
 			str = gettupleitem(v, 0);
 			globals = gettupleitem(v, 1);
-			if (n == 3)
+			if (n == 3) {
 				locals = gettupleitem(v, 2);
+            }
 		}
 	}
-	if (str == NULL || !is_stringobject(str) ||
-			globals != NULL && !is_dictobject(globals) ||
-			locals != NULL && !is_dictobject(locals)) {
+	if (str == NULL || !is_stringobject(str) || globals != NULL
+        && !is_dictobject(globals) || locals != NULL
+        && !is_dictobject(locals))
+    {
 		err_setstr(TypeError,
-		    "exec/eval arguments must be string[,dict[,dict]]");
+                   "exec/eval arguments must be string[,dict[,dict]]");
 		return NULL;
 	}
 	return run_string(getstringvalue(str), start, globals, locals);
 }
 
 static object *
-builtin_eval(self, v)
-	object *self;
-	object *v;
+builtin_eval(object *self, object *v)
 {
 	return exec_eval(v, eval_input);
 }
 
 static object *
-builtin_exec(self, v)
-	object *self;
-	object *v;
+builtin_exec(object *self, object *v)
 {
 	return exec_eval(v, file_input);
 }
 
 static object *
-builtin_float(self, v)
-	object *self;
-	object *v;
+builtin_float(object *self, object *v)
 {
 	if (v == NULL) {
 		/* */
@@ -197,27 +188,25 @@ builtin_float(self, v)
 }
 
 static object *
-builtin_input(self, v)
-	object *self;
-	object *v;
+builtin_input(object *self, object *v)
 {
 	FILE *in = sysgetfile("stdin", stdin);
 	FILE *out = sysgetfile("stdout", stdout);
 	node *n;
 	int err;
 	object *m, *d;
+
 	flushline();
-	if (v != NULL)
+	if (v != NULL) {
 		printobject(v, out, PRINT_RAW);
+    }
 	m = add_module("__main__");
 	d = getmoduledict(m);
 	return run_file(in, "<stdin>", expr_input, d, d);
 }
 
 static object *
-builtin_int(self, v)
-	object *self;
-	object *v;
+builtin_int(object *self, object *v)
 {
 	if (v == NULL) {
 		/* */
@@ -235,12 +224,11 @@ builtin_int(self, v)
 }
 
 static object *
-builtin_len(self, v)
-	object *self;
-	object *v;
+builtin_len(object *self, object *v)
 {
 	long len;
 	typeobject *tp;
+
 	if (v == NULL) {
 		err_setstr(TypeError, "len() without argument");
 		return NULL;
@@ -260,13 +248,12 @@ builtin_len(self, v)
 }
 
 static object *
-min_max(v, sign)
-	object *v;
-	int sign;
+min_max(object *v, int sign)
 {
 	int i, n, cmp;
 	object *w, *x;
 	sequence_methods *sq;
+
 	if (v == NULL) {
 		err_setstr(TypeError, "min() or max() without argument");
 		return NULL;
@@ -289,37 +276,34 @@ min_max(v, sign)
 			DECREF(w);
 			w = x;
 		}
-		else
+		else {
 			DECREF(x);
+        }
 	}
 	return w;
 }
 
 static object *
-builtin_min(self, v)
-	object *self;
-	object *v;
+builtin_min(object *self, object *v)
 {
 	return min_max(v, -1);
 }
 
 static object *
-builtin_max(self, v)
-	object *self;
-	object *v;
+builtin_max(object *self, object *v)
 {
 	return min_max(v, 1);
 }
 
 static object *
-builtin_open(self, v)
-	object *self;
-	object *v;
+builtin_open(object *self, object *v)
 {
 	object *name, *mode;
-	if (v == NULL || !is_tupleobject(v) || gettuplesize(v) != 2 ||
-		!is_stringobject(name = gettupleitem(v, 0)) ||
-		!is_stringobject(mode = gettupleitem(v, 1))) {
+
+	if (v == NULL || !is_tupleobject(v) || gettuplesize(v) != 2
+        || !is_stringobject(name = gettupleitem(v, 0))
+        || !is_stringobject(mode = gettupleitem(v, 1)))
+    {
 		err_setstr(TypeError, "open() requires 2 string arguments");
 		return NULL;
 	}
@@ -328,9 +312,7 @@ builtin_open(self, v)
 }
 
 static object *
-builtin_ord(self, v)
-	object *self;
-	object *v;
+builtin_ord(object *self, object *v)
 {
 	if (v == NULL || !is_stringobject(v)) {
 		err_setstr(TypeError, "ord() must have string argument");
@@ -344,15 +326,16 @@ builtin_ord(self, v)
 }
 
 static object *
-builtin_range(self, v)
-	object *self;
-	object *v;
+builtin_range(object *self, object *v)
 {
 	static char *errmsg = "range() requires 1-3 int arguments";
 	int i, n;
 	long ilow, ihigh, istep;
+
 	if (v != NULL && is_intobject(v)) {
-		ilow = 0; ihigh = getintvalue(v); istep = 1;
+		ilow = 0;
+        ihigh = getintvalue(v);
+        istep = 1;
 	}
 	else if (v == NULL || !is_tupleobject(v)) {
 		err_setstr(TypeError, errmsg);
@@ -374,28 +357,35 @@ builtin_range(self, v)
 			istep = getintvalue(gettupleitem(v, 2));
 			--n;
 		}
-		else
+		else {
 			istep = 1;
+        }
 		ihigh = getintvalue(gettupleitem(v, --n));
-		if (n > 0)
+		if (n > 0) {
 			ilow = getintvalue(gettupleitem(v, 0));
-		else
+        }
+		else {
 			ilow = 0;
+        }
 	}
 	if (istep == 0) {
 		err_setstr(RuntimeError, "zero step for range()");
 		return NULL;
 	}
 	/* XXX ought to check overflow of subtraction */
-	if (istep > 0)
+	if (istep > 0) {
 		n = (ihigh - ilow + istep - 1) / istep;
-	else
+    }
+	else {
 		n = (ihigh - ilow + istep + 1) / istep;
-	if (n < 0)
+    }
+	if (n < 0) {
 		n = 0;
+    }
 	v = newlistobject(n);
-	if (v == NULL)
+	if (v == NULL) {
 		return NULL;
+    }
 	for (i = 0; i < n; i++) {
 		object *w = newintobject(ilow);
 		if (w == NULL) {
@@ -409,29 +399,30 @@ builtin_range(self, v)
 }
 
 static object *
-builtin_raw_input(self, v)
-	object *self;
-	object *v;
+builtin_raw_input(object *self, object *v)
 {
 	FILE *in = sysgetfile("stdin", stdin);
 	FILE *out = sysgetfile("stdout", stdout);
 	char *p;
 	int err;
 	int n = 1000;
+
 	flushline();
-	if (v != NULL)
+	if (v != NULL) {
 		printobject(v, out, PRINT_RAW);
+    }
 	v = newsizedstringobject((char *)NULL, n);
 	if (v != NULL) {
-		if ((err = fgets_intr(getstringvalue(v), n+1, in)) != E_OK) {
+		if ((err = fgets_intr(getstringvalue(v), n + 1, in)) != E_OK) {
 			err_input(err);
 			DECREF(v);
 			return NULL;
 		}
 		else {
 			n = strlen(getstringvalue(v));
-			if (n > 0 && getstringvalue(v)[n-1] == '\n')
+			if (n > 0 && getstringvalue(v)[n - 1] == '\n') {
 				n--;
+            }
 			resizestring(&v, n);
 		}
 	}
@@ -439,17 +430,13 @@ builtin_raw_input(self, v)
 }
 
 static object *
-builtin_reload(self, v)
-	object *self;
-	object *v;
+builtin_reload(object *self, object *v)
 {
 	return reload_module(v);
 }
 
 static object *
-builtin_type(self, v)
-	object *self;
-	object *v;
+builtin_type(object *self, object *v)
 {
 	if (v == NULL) {
 		err_setstr(TypeError, "type() requres an argument");
@@ -461,38 +448,36 @@ builtin_type(self, v)
 }
 
 static struct methodlist builtin_methods[] = {
-	{"abs", builtin_abs},
-	{"chr", builtin_chr},
-	{"dir", builtin_dir},
-	{"divmod", builtin_divmod},
-	{"eval", builtin_eval},
-	{"exec", builtin_exec},
-	{"float", builtin_float},
-	{"input", builtin_input},
-	{"int", builtin_int},
-	{"len", builtin_len},
-	{"max", builtin_max},
-	{"min", builtin_min},
-	{"open", builtin_open}, /* XXX move to OS module */
-	{"ord", builtin_ord},
-	{"range", builtin_range},
-	{"raw_input", builtin_raw_input},
-	{"reload", builtin_reload},
-	{"type", builtin_type},
-	{NULL, NULL},
+	{"abs", 		builtin_abs},
+	{"chr", 		builtin_chr},
+	{"dir", 		builtin_dir},
+	{"divmod", 		builtin_divmod},
+	{"eval", 		builtin_eval},
+	{"exec", 		builtin_exec},
+	{"float", 		builtin_float},
+	{"input", 		builtin_input},
+	{"int",		    builtin_int},
+	{"len", 		builtin_len},
+	{"max", 		builtin_max},
+	{"min", 		builtin_min},
+	{"open", 		builtin_open}, /* XXX move to OS module */
+	{"ord", 		builtin_ord},
+	{"range", 		builtin_range},
+	{"raw_input",	builtin_raw_input},
+	{"reload", 		builtin_reload},
+	{"type", 		builtin_type},
+	{NULL, 			NULL},
 };
 
 static object *builtin_dict;
 
 object *
-getbuiltin(name)
-	char *name;
+getbuiltin(char *name)
 {
 	return dictlookup(builtin_dict, name);
 }
 
 /* Predefined exceptions */
-
 object *RuntimeError;
 object *EOFError;
 object *TypeError;
@@ -502,26 +487,27 @@ object *SystemError;
 object *KeyboardInterrupt;
 
 static object *
-newstdexception(name, message)
-	char *name, *message;
+newstdexception(char *name, char *message)
 {
 	object *v = newstringobject(message);
-	if (v == NULL || dictinsert(builtin_dict, name, v) != 0)
+
+	if (v == NULL || dictinsert(builtin_dict, name, v) != 0) {
 		fatal("no mem for new standard exception");
+    }
 	return v;
 }
 
 static void
 initerrors()
 {
-	RuntimeError = newstdexception("RuntimeError", "run-time error");
-	EOFError = newstdexception("EOFError", "end-of-file read");
-	TypeError = newstdexception("TypeError", "type error");
-	MemoryError = newstdexception("MemoryError", "out of memory");
-	NameError = newstdexception("NameError", "undefined name");
-	SystemError = newstdexception("SystemError", "system error");
-	KeyboardInterrupt =
-		newstdexception("KeyboardInterrupt", "keyboard interrupt");
+	RuntimeError 		= 	newstdexception("RuntimeError", "run-time error");
+	EOFError 			= 	newstdexception("EOFError", "end-of-file read");
+	TypeError 			= 	newstdexception("TypeError", "type error");
+	MemoryError 		= 	newstdexception("MemoryError", "out of memory");
+	NameError 			= 	newstdexception("NameError", "undefined name");
+	SystemError 		= 	newstdexception("SystemError", "system error");
+	KeyboardInterrupt	= 	newstdexception("KeyboardInterrupt",
+                                        	"keyboard interrupt");
 }
 
 void
@@ -530,7 +516,8 @@ initbuiltin()
 	object *m;
 	m = initmodule("builtin", builtin_methods);
 	builtin_dict = getmoduledict(m);
+
 	INCREF(builtin_dict);
 	initerrors();
-	(void) dictinsert(builtin_dict, "None", None);
+	(void)dictinsert(builtin_dict, "None", None);
 }
