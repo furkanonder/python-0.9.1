b83be7ba350e128663d2ff2caa9788d8c24dc020
Furkan Onder <furkanonder@protonmail.com>
2024-10-30 23:55:14 +0300

    Convert K&R style function declarations to ANSI C style in acceler.c

--- src/acceler.c
+++ src/acceler.c
@@ -19,8 +19,7 @@ static void fixdfa PROTO((grammar *, dfa *));
 static void fixstate PROTO((grammar *, dfa *, state *));
 
 void
-addaccelerators(g)
-	grammar *g;
+addaccelerators(grammar *g)
 {
 	dfa *d;
 	int i;
@@ -28,8 +27,10 @@ addaccelerators(g)
 	printf("Adding parser accellerators ...\n");
 #endif
 	d = g->g_dfa;
-	for (i = g->g_ndfas; --i >= 0; d++)
+
+	for (i = g->g_ndfas; --i >= 0; d++) {
 		fixdfa(g, d);
+    }
 	g->g_accel = 1;
 #ifdef DEBUG
 	printf("Done.\n");
@@ -37,22 +38,19 @@ addaccelerators(g)
 }
 
 static void
-fixdfa(g, d)
-	grammar *g;
-	dfa *d;
+fixdfa(grammar *g, dfa *d)
 {
 	state *s;
 	int j;
 	s = d->d_state;
-	for (j = 0; j < d->d_nstates; j++, s++)
+
+	for (j = 0; j < d->d_nstates; j++, s++) {
 		fixstate(g, d, s);
+    }
 }
 
 static void
-fixstate(g, d, s)
-	grammar *g;
-	dfa *d;
-	state *s;
+fixstate(grammar *g, dfa *d, state *s)
 {
 	arc *a;
 	int k;
@@ -60,8 +58,10 @@ fixstate(g, d, s)
 	int nl = g->g_ll.ll_nlabels;
 	s->s_accept = 0;
 	accel = NEW(int, nl);
-	for (k = 0; k < nl; k++)
+
+	for (k = 0; k < nl; k++) {
 		accel[k] = -1;
+    }
 	a = s->s_arc;
 	for (k = s->s_narcs; --k >= 0; a++) {
 		int lbl = a->a_lbl;
@@ -80,33 +80,39 @@ fixstate(g, d, s)
 			}
 			for (ibit = 0; ibit < g->g_ll.ll_nlabels; ibit++) {
 				if (testbit(d1->d_first, ibit)) {
-					if (accel[ibit] != -1)
+					if (accel[ibit] != -1) {
 						printf("XXX ambiguity!\n");
-					accel[ibit] = a->a_arrow | (1 << 7) |
-						((type - NT_OFFSET) << 8);
+                    }
+					accel[ibit] = a->a_arrow | (1 << 7)
+                                  | ((type - NT_OFFSET) << 8);
 				}
 			}
 		}
-		else if (lbl == EMPTY)
+		else if (lbl == EMPTY) {
 			s->s_accept = 1;
-		else if (lbl >= 0 && lbl < nl)
+        }
+		else if (lbl >= 0 && lbl < nl) {
 			accel[lbl] = a->a_arrow;
+        }
 	}
-	while (nl > 0 && accel[nl-1] == -1)
+	while (nl > 0 && accel[nl - 1] == -1) {
 		nl--;
-	for (k = 0; k < nl && accel[k] == -1;)
+    }
+	for (k = 0; k < nl && accel[k] == -1;) {
 		k++;
+    }
 	if (k < nl) {
 		int i;
-		s->s_accel = NEW(int, nl-k);
+		s->s_accel = NEW(int, nl - k);
 		if (s->s_accel == NULL) {
 			fprintf(stderr, "no mem to add parser accelerators\n");
 			exit(1);
 		}
 		s->s_lower = k;
 		s->s_upper = nl;
-		for (i = 0; k < nl; i++, k++)
+		for (i = 0; k < nl; i++, k++) {
 			s->s_accel[i] = accel[k];
+        }
 	}
 	DEL(accel);
 }
