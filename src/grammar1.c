37a9907ada402b3c8c22a65d2809afb15b5bb31d
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 02:23:21 +0300

    Convert K&R style function declarations to ANSI C style in grammar1.c

--- src/grammar1.c
+++ src/grammar1.c
@@ -8,43 +8,42 @@
 /* Return the DFA for the given type */
 
 dfa *
-finddfa(g, type)
-	grammar *g;
-	register int type;
+finddfa(grammar *g, register int type)
 {
 	register int i;
 	register dfa *d;
 	
 	for (i = g->g_ndfas, d = g->g_dfa; --i >= 0; d++) {
-		if (d->d_type == type)
+		if (d->d_type == type) {
 			return d;
+        }
 	}
 	assert(0);
 	/* NOTREACHED */
 }
 
 char *
-labelrepr(lb)
-	label *lb;
+labelrepr(label *lb)
 {
 	static char buf[100];
 	
-	if (lb->lb_type == ENDMARKER)
+	if (lb->lb_type == ENDMARKER) {
 		return "EMPTY";
+    }
 	else if (ISNONTERMINAL(lb->lb_type)) {
 		if (lb->lb_str == NULL) {
 			sprintf(buf, "NT%d", lb->lb_type);
 			return buf;
 		}
-		else
+		else {
 			return lb->lb_str;
+        }
 	}
 	else {
 		if (lb->lb_str == NULL)
 			return tok_name[lb->lb_type];
 		else {
-			sprintf(buf, "%.32s(%.32s)",
-				tok_name[lb->lb_type], lb->lb_str);
+			sprintf(buf, "%.32s(%.32s)", tok_name[lb->lb_type], lb->lb_str);
 			return buf;
 		}
 	}
