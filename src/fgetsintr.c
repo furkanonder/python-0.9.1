3ac3edcf68c77dc661df6a020bac9241ecb608f0
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 00:57:22 +0300

    Convert K&R style function declarations to ANSI C style in fgetsintr.c

--- src/fgetsintr.c
+++ src/fgetsintr.c
@@ -15,7 +15,7 @@
 #include "intrcheck.h"
 
 #ifndef AMOEBA
-#define sig_block()	/*empty*/
+#define sig_block()		/*empty*/
 #define sig_unblock()	/*empty*/
 #endif
 
@@ -24,17 +24,13 @@ static jmp_buf jback;
 static void catcher PROTO((int));
 
 static void
-catcher(sig)
-	int sig;
+catcher(int sig)
 {
 	longjmp(jback, 1);
 }
 
 int
-fgets_intr(buf, size, fp)
-	char *buf;
-	int size;
-	FILE *fp;
+fgets_intr(char *buf, int size, FILE *fp)
 {
 	int ret;
 	SIGTYPE (*sigsave)();
@@ -51,12 +47,13 @@ fgets_intr(buf, size, fp)
 	/* The casts to (SIGTYPE(*)()) are needed by THINK_C only */
 	
 	sigsave = signal(SIGINT, (SIGTYPE(*)()) SIG_IGN);
-	if (sigsave != (SIGTYPE(*)()) SIG_IGN)
+	if (sigsave != (SIGTYPE(*)()) SIG_IGN) {
 		signal(SIGINT, (SIGTYPE(*)()) catcher);
-	
+	}
 #ifndef THINK_C
-	if (intrcheck())
+	if (intrcheck()) {
 		ret = E_INTR;
+    }
 	else
 #endif
 	{
@@ -65,7 +62,8 @@ fgets_intr(buf, size, fp)
 		sig_unblock();
 	}
 	
-	if (sigsave != (SIGTYPE(*)()) SIG_IGN)
+	if (sigsave != (SIGTYPE(*)()) SIG_IGN) {
 		signal(SIGINT, (SIGTYPE(*)()) sigsave);
+    }
 	return ret;
 }
