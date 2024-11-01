ddc5f0781d01f7409d55f3e25d6f7c61820d020a
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 02:31:31 +0300

    Convert K&R style function declarations to ANSI C style in intobject.c

--- src/intobject.c
+++ src/intobject.c
@@ -38,19 +38,21 @@ err_zdiv()
    dedicated free list, filled when necessary with memory from malloc().
 */
 
-#define BLOCK_SIZE	1000	/* 1K less typical malloc overhead */
+#define BLOCK_SIZE		1000	/* 1K less typical malloc overhead */
 #define N_INTOBJECTS	(BLOCK_SIZE / sizeof(intobject))
 
 static intobject *
 fill_free_list()
 {
-	intobject *p, *q;
-	p = NEW(intobject, N_INTOBJECTS);
-	if (p == NULL)
+	intobject *p = NEW(intobject, N_INTOBJECTS), *q;
+
+	if (p == NULL) {
 		return (intobject *)err_nomem();
+    }
 	q = p + N_INTOBJECTS;
-	while (--q > p)
-		*(intobject **)q = q-1;
+	while (--q > p) {
+		*(intobject **)q = q - 1;
+    }
 	*(intobject **)q = NULL;
 	return p + N_INTOBJECTS - 1;
 }
@@ -58,13 +60,14 @@ fill_free_list()
 static intobject *free_list = NULL;
 
 object *
-newintobject(ival)
-	long ival;
+newintobject(long ival)
 {
 	register intobject *v;
+
 	if (free_list == NULL) {
-		if ((free_list = fill_free_list()) == NULL)
+		if ((free_list = fill_free_list()) == NULL) {
 			return NULL;
+        }
 	}
 	v = free_list;
 	free_list = *(intobject **)free_list;
@@ -75,97 +78,92 @@ newintobject(ival)
 }
 
 static void
-int_dealloc(v)
-	intobject *v;
+int_dealloc(intobject *v)
 {
 	*(intobject **)v = free_list;
 	free_list = v;
 }
 
 long
-getintvalue(op)
-	register object *op;
+getintvalue(register object *op)
 {
 	if (!is_intobject(op)) {
 		err_badcall();
 		return -1;
 	}
-	else
-		return ((intobject *)op) -> ob_ival;
+	else {
+		return ((intobject *)op)->ob_ival;
+    }
 }
 
 /* Methods */
 
 static void
-int_print(v, fp, flags)
-	intobject *v;
-	FILE *fp;
-	int flags;
+int_print(intobject *v, FILE *fp, int flags)
 {
 	fprintf(fp, "%ld", v->ob_ival);
 }
 
 static object *
-int_repr(v)
-	intobject *v;
+int_repr(intobject *v)
 {
 	char buf[20];
+
 	sprintf(buf, "%ld", v->ob_ival);
 	return newstringobject(buf);
 }
 
 static int
-int_compare(v, w)
-	intobject *v, *w;
+int_compare(intobject *v, intobject *w)
 {
 	register long i = v->ob_ival;
 	register long j = w->ob_ival;
+
 	return (i < j) ? -1 : (i > j) ? 1 : 0;
 }
 
 static object *
-int_add(v, w)
-	intobject *v;
-	register object *w;
+int_add(intobject *v, register object *w)
 {
 	register long a, b, x;
+
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
 	}
 	a = v->ob_ival;
-	b = ((intobject *)w) -> ob_ival;
+	b = ((intobject *)w)->ob_ival;
 	x = a + b;
-	if ((x^a) < 0 && (x^b) < 0)
+	if ((x ^ a) < 0 && (x ^ b) < 0) {
 		return err_ovf();
+    }
 	return newintobject(x);
 }
 
 static object *
-int_sub(v, w)
-	intobject *v;
-	register object *w;
+int_sub(intobject *v, register object *w)
 {
 	register long a, b, x;
+
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
 	}
 	a = v->ob_ival;
-	b = ((intobject *)w) -> ob_ival;
+	b = ((intobject *)w)->ob_ival;
 	x = a - b;
-	if ((x^a) < 0 && (x^~b) < 0)
+	if ((x ^ a) < 0 && (x ^~ b) < 0) {
 		return err_ovf();
+    }
 	return newintobject(x);
 }
 
 static object *
-int_mul(v, w)
-	intobject *v;
-	register object *w;
+int_mul(intobject *v, register object *w)
 {
 	register long a, b;
 	double x;
+
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
@@ -173,46 +171,44 @@ int_mul(v, w)
 	a = v->ob_ival;
 	b = ((intobject *)w) -> ob_ival;
 	x = (double)a * (double)b;
-	if (x > 0x7fffffff || x < (double) (long) 0x80000000)
+	if (x > 0x7fffffff || x < (double)(long)0x80000000) {
 		return err_ovf();
+    }
 	return newintobject(a * b);
 }
 
 static object *
-int_div(v, w)
-	intobject *v;
-	register object *w;
+int_div(intobject *v, register object *w)
 {
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	if (((intobject *)w) -> ob_ival == 0)
+	if (((intobject *)w)->ob_ival == 0) {
 		return err_zdiv();
-	return newintobject(v->ob_ival / ((intobject *)w) -> ob_ival);
+    }
+	return newintobject(v->ob_ival / ((intobject *)w)->ob_ival);
 }
 
 static object *
-int_rem(v, w)
-	intobject *v;
-	register object *w;
+int_rem(intobject *v, register object *w)
 {
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	if (((intobject *)w) -> ob_ival == 0)
+	if (((intobject *)w)->ob_ival == 0) {
 		return err_zdiv();
-	return newintobject(v->ob_ival % ((intobject *)w) -> ob_ival);
+    }
+	return newintobject(v->ob_ival % ((intobject *)w)->ob_ival);
 }
 
 static object *
-int_pow(v, w)
-	intobject *v;
-	register object *w;
+int_pow(intobject *v, register object *w)
 {
 	register long iv, iw, ix;
 	register int neg;
+
 	if (!is_intobject(w)) {
 		err_badarg();
 		return NULL;
@@ -220,49 +216,51 @@ int_pow(v, w)
 	iv = v->ob_ival;
 	iw = ((intobject *)w)->ob_ival;
 	neg = 0;
-	if (iw < 0)
+	if (iw < 0) {
 		neg = 1, iw = -iw;
+    }
 	ix = 1;
-	for (; iw > 0; iw--)
+	for (; iw > 0; iw--) {
 		ix = ix * iv;
+    }
 	if (neg) {
-		if (ix == 0)
+		if (ix == 0) {
 			return err_zdiv();
-		ix = 1/ix;
+        }
+		ix = 1 / ix;
 	}
 	/* XXX How to check for overflow? */
 	return newintobject(ix);
 }
 
 static object *
-int_neg(v)
-	intobject *v;
+int_neg(intobject *v)
 {
-	register long a, x;
-	a = v->ob_ival;
+	register long a = v->ob_ival, x;
 	x = -a;
-	if (a < 0 && x < 0)
+
+	if (a < 0 && x < 0) {
 		return err_ovf();
+    }
 	return newintobject(x);
 }
 
 static object *
-int_pos(v)
-	intobject *v;
+int_pos(intobject *v)
 {
 	INCREF(v);
 	return (object *)v;
 }
 
 static number_methods int_as_number = {
-	int_add,	/*tp_add*/
-	int_sub,	/*tp_subtract*/
-	int_mul,	/*tp_multiply*/
-	int_div,	/*tp_divide*/
-	int_rem,	/*tp_remainder*/
-	int_pow,	/*tp_power*/
-	int_neg,	/*tp_negate*/
-	int_pos,	/*tp_plus*/
+	(binaryfunc)int_add,	/*tp_add*/
+	(binaryfunc)int_sub,	/*tp_subtract*/
+	(binaryfunc)int_mul,	/*tp_multiply*/
+	(binaryfunc)int_div,	/*tp_divide*/
+	(binaryfunc)int_rem,	/*tp_remainder*/
+	(ternaryfunc)int_pow,	/*tp_power*/
+	(unaryfunc)int_neg,		/*tp_negate*/
+	(unaryfunc)int_pos,		/*tp_plus*/
 };
 
 typeobject Inttype = {
@@ -271,13 +269,13 @@ typeobject Inttype = {
 	"int",
 	sizeof(intobject),
 	0,
-	int_dealloc,	/*tp_dealloc*/
-	int_print,	/*tp_print*/
-	0,		/*tp_getattr*/
-	0,		/*tp_setattr*/
-	int_compare,	/*tp_compare*/
-	int_repr,	/*tp_repr*/
-	&int_as_number,	/*tp_as_number*/
-	0,		/*tp_as_sequence*/
-	0,		/*tp_as_mapping*/
+	(destructor)int_dealloc,	/*tp_dealloc*/
+	(printfunc)int_print,		/*tp_print*/
+	0,							/*tp_getattr*/
+	0,							/*tp_setattr*/
+	(cmpfunc)int_compare,		/*tp_compare*/
+	(reprfunc)int_repr,			/*tp_repr*/
+	&int_as_number,				/*tp_as_number*/
+	0,							/*tp_as_sequence*/
+	0,							/*tp_as_mapping*/
 };
