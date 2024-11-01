680c126be404565b0e407fdd98796d7b1242bbbf
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 01:06:05 +0300

    Convert K&R style function declarations to ANSI C style in fileobject.c

--- src/fileobject.c
+++ src/fileobject.c
@@ -15,15 +15,14 @@ extern int errno;
 
 typedef struct {
 	OB_HEAD
-	FILE *f_fp;
-	object *f_name;
-	object *f_mode;
+	FILE 	*f_fp;
+	object	*f_name;
+	object	*f_mode;
 	/* XXX Should move the 'need space' on printing flag here */
 } fileobject;
 
 FILE *
-getfilefile(f)
-	object *f;
+getfilefile(object *f)
 {
 	if (!is_fileobject(f)) {
 		err_badcall();
@@ -33,14 +32,13 @@ getfilefile(f)
 }
 
 object *
-newopenfileobject(fp, name, mode)
-	FILE *fp;
-	char *name;
-	char *mode;
+newopenfileobject(FILE *fp, char *name, char *mode)
 {
 	fileobject *f = NEWOBJ(fileobject, &Filetype);
-	if (f == NULL)
+
+	if (f == NULL) {
 		return NULL;
+    }
 	f->f_fp = NULL;
 	f->f_name = newstringobject(name);
 	f->f_mode = newstringobject(mode);
@@ -49,22 +47,23 @@ newopenfileobject(fp, name, mode)
 		return NULL;
 	}
 	f->f_fp = fp;
-	return (object *) f;
+	return (object *)f;
 }
 
 object *
-newfileobject(name, mode)
-	char *name, *mode;
+newfileobject(char *name, char *mode)
 {
 	fileobject *f;
 	FILE *fp;
-	f = (fileobject *) newopenfileobject((FILE *)NULL, name, mode);
-	if (f == NULL)
+	f = (fileobject *)newopenfileobject((FILE *)NULL, name, mode);
+
+	if (f == NULL) {
 		return NULL;
+    }
 #ifdef THINK_C
 	if (*mode == '*') {
 		FILE *fopenRF();
-		f->f_fp = fopenRF(name, mode+1);
+		f->f_fp = fopenRF(name, mode + 1);
 	}
 	else
 #endif
@@ -80,23 +79,22 @@ newfileobject(name, mode)
 /* Methods */
 
 static void
-file_dealloc(f)
-	fileobject *f;
+file_dealloc(fileobject *f)
 {
-	if (f->f_fp != NULL)
+	if (f->f_fp != NULL) {
 		fclose(f->f_fp);
-	if (f->f_name != NULL)
+    }
+	if (f->f_name != NULL) {
 		DECREF(f->f_name);
-	if (f->f_mode != NULL)
+    }
+	if (f->f_mode != NULL) {
 		DECREF(f->f_mode);
+    }
 	free((char *)f);
 }
 
 static void
-file_print(f, fp, flags)
-	fileobject *f;
-	FILE *fp;
-	int flags;
+file_print(fileobject *f, FILE *fp, int flags)
 {
 	fprintf(fp, "<%s file ", f->f_fp == NULL ? "closed" : "open");
 	printobject(f->f_name, fp, flags);
@@ -106,23 +104,20 @@ file_print(f, fp, flags)
 }
 
 static object *
-file_repr(f)
-	fileobject *f;
+file_repr(fileobject *f)
 {
 	char buf[300];
+
 	/* XXX This differs from file_print if the filename contains
 	   quotes or other funny characters. */
 	sprintf(buf, "<%s file '%.256s', mode '%.10s'>",
-		f->f_fp == NULL ? "closed" : "open",
-		getstringvalue(f->f_name),
-		getstringvalue(f->f_mode));
+            f->f_fp == NULL ? "closed" : "open", getstringvalue(f->f_name),
+			getstringvalue(f->f_mode));
 	return newstringobject(buf);
 }
 
 static object *
-file_close(f, args)
-	fileobject *f;
-	object *args;
+file_close(fileobject *f, object *args)
 {
 	if (args != NULL) {
 		err_badarg();
@@ -137,12 +132,11 @@ file_close(f, args)
 }
 
 static object *
-file_read(f, args)
-	fileobject *f;
-	object *args;
+file_read(fileobject *f, object *args)
 {
 	int n;
 	object *v;
+
 	if (f->f_fp == NULL) {
 		err_badarg();
 		return NULL;
@@ -157,8 +151,9 @@ file_read(f, args)
 		return NULL;
 	}
 	v = newsizedstringobject((char *)NULL, n);
-	if (v == NULL)
+	if (v == NULL) {
 		return NULL;
+    }
 	n = fread(getstringvalue(v), 1, n, f->f_fp);
 	/* EOF is reported as an empty string */
 	/* XXX should detect real I/O errors? */
@@ -169,12 +164,11 @@ file_read(f, args)
 /* XXX Should this be unified with raw_input()? */
 
 static object *
-file_readline(f, args)
-	fileobject *f;
-	object *args;
+file_readline(fileobject *f, object *args)
 {
 	int n;
 	object *v;
+
 	if (f->f_fp == NULL) {
 		err_badarg();
 		return NULL;
@@ -194,11 +188,12 @@ file_readline(f, args)
 		return NULL;
 	}
 	v = newsizedstringobject((char *)NULL, n);
-	if (v == NULL)
+	if (v == NULL) {
 		return NULL;
+    }
 #ifndef THINK_C_3_0
 	/* XXX Think C 3.0 wrongly reads up to n characters... */
-	n = n+1;
+	n = n + 1;
 #endif
 	if (fgets(getstringvalue(v), n, f->f_fp) == NULL) {
 		/* EOF is reported as an empty string */
@@ -213,11 +208,10 @@ file_readline(f, args)
 }
 
 static object *
-file_write(f, args)
-	fileobject *f;
-	object *args;
+file_write(fileobject *f, object *args)
 {
 	int n, n2;
+
 	if (f->f_fp == NULL) {
 		err_badarg();
 		return NULL;
@@ -229,8 +223,9 @@ file_write(f, args)
 	errno = 0;
 	n2 = fwrite(getstringvalue(args), 1, n = getstringsize(args), f->f_fp);
 	if (n2 != n) {
-		if (errno == 0)
+		if (errno == 0) {
 			errno = EIO;
+        }
 		err_errno(RuntimeError);
 		return NULL;
 	}
@@ -239,17 +234,15 @@ file_write(f, args)
 }
 
 static struct methodlist file_methods[] = {
-	{"write",	file_write},
-	{"read",	file_read},
-	{"readline",	file_readline},
-	{"close",	file_close},
-	{NULL,		NULL}		/* sentinel */
+	{"write",		(method)file_write},
+	{"read",		(method)file_read},
+	{"readline",	(method)file_readline},
+	{"close",		(method)file_close},
+	{NULL,			NULL}	/* sentinel */
 };
 
 static object *
-file_getattr(f, name)
-	fileobject *f;
-	char *name;
+file_getattr(fileobject *f, char *name)
 {
 	return findmethod(file_methods, (object *)f, name);
 }
@@ -260,10 +253,10 @@ typeobject Filetype = {
 	"file",
 	sizeof(fileobject),
 	0,
-	file_dealloc,	/*tp_dealloc*/
-	file_print,	/*tp_print*/
-	file_getattr,	/*tp_getattr*/
-	0,		/*tp_setattr*/
-	0,		/*tp_compare*/
-	file_repr,	/*tp_repr*/
+	(destructor)file_dealloc,	/*tp_dealloc*/
+	(printfunc)file_print,		/*tp_print*/
+	(getattrfunc)file_getattr,	/*tp_getattr*/
+	0,							/*tp_setattr*/
+	0,							/*tp_compare*/
+	(reprfunc)file_repr,		/*tp_repr*/
 };
