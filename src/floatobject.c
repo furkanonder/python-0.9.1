8924f03ab30ae46e123db5a8cd7b2df71ee632e9
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 01:32:56 +0300

    Convert K&R style function declarations to ANSI C style in floatobject.c

--- src/floatobject.c
+++ src/floatobject.c
@@ -19,46 +19,45 @@ extern double pow PROTO((double, double));
 #endif
 
 object *
-newfloatobject(fval)
-	double fval;
+newfloatobject(double fval)
 {
 	/* For efficiency, this code is copied from newobject() */
-	register floatobject *op = (floatobject *) malloc(sizeof(floatobject));
-	if (op == NULL)
+	register floatobject *op = (floatobject *)malloc(sizeof(floatobject));
+
+	if (op == NULL) {
 		return err_nomem();
+    }
 	NEWREF(op);
 	op->ob_type = &Floattype;
 	op->ob_fval = fval;
-	return (object *) op;
+	return (object *)op;
 }
 
 void
-float_dealloc(op)
-	object *op;
+float_dealloc(floatobject *op)
 {
 	DEL(op);
 }
 
 double
-getfloatvalue(op)
-	object *op;
+getfloatvalue(object *op)
 {
 	if (!is_floatobject(op)) {
 		err_badarg();
 		return -1;
 	}
-	else
-		return ((floatobject *)op) -> ob_fval;
+	else {
+		return ((floatobject *)op)->ob_fval;
+    }
 }
 
 /* Methods */
 
 static void
-float_buf_repr(buf, v)
-	char *buf;
-	floatobject *v;
+float_buf_repr(char *buf, floatobject *v)
 {
 	register char *cp;
+
 	/* Subroutine for float_repr and float_print.
 	   We want float numbers to be recognizable as such,
 	   i.e., they should contain a decimal point or an exponent.
@@ -66,13 +65,15 @@ float_buf_repr(buf, v)
 	   in such cases, we append ".0" to the string. */
 	sprintf(buf, "%.12g", v->ob_fval);
 	cp = buf;
-	if (*cp == '-')
+	if (*cp == '-') {
 		cp++;
+    }
 	for (; *cp != '\0'; cp++) {
 		/* Any non-digit means it's not an integer;
 		   this takes care of NAN and INF as well. */
-		if (!isdigit(*cp))
+		if (!isdigit(*cp)) {
 			break;
+        }
 	}
 	if (*cp == '\0') {
 		*cp++ = '.';
@@ -82,97 +83,86 @@ float_buf_repr(buf, v)
 }
 
 static void
-float_print(v, fp, flags)
-	floatobject *v;
-	FILE *fp;
-	int flags;
+float_print(floatobject *v, FILE *fp, int flags)
 {
 	char buf[100];
+
 	float_buf_repr(buf, v);
 	fputs(buf, fp);
 }
 
 static object *
-float_repr(v)
-	floatobject *v;
+float_repr(floatobject *v)
 {
 	char buf[100];
+
 	float_buf_repr(buf, v);
 	return newstringobject(buf);
 }
 
 static int
-float_compare(v, w)
-	floatobject *v, *w;
+float_compare(floatobject *v, floatobject *w)
 {
 	double i = v->ob_fval;
 	double j = w->ob_fval;
+
 	return (i < j) ? -1 : (i > j) ? 1 : 0;
 }
 
 static object *
-float_add(v, w)
-	floatobject *v;
-	object *w;
+float_add(floatobject *v, object *w)
 {
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	return newfloatobject(v->ob_fval + ((floatobject *)w) -> ob_fval);
+	return newfloatobject(v->ob_fval + ((floatobject *)w)->ob_fval);
 }
 
 static object *
-float_sub(v, w)
-	floatobject *v;
-	object *w;
+float_sub(floatobject *v, object *w)
 {
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	return newfloatobject(v->ob_fval - ((floatobject *)w) -> ob_fval);
+	return newfloatobject(v->ob_fval - ((floatobject *)w)->ob_fval);
 }
 
 static object *
-float_mul(v, w)
-	floatobject *v;
-	object *w;
+float_mul(floatobject *v, object *w)
 {
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	return newfloatobject(v->ob_fval * ((floatobject *)w) -> ob_fval);
+	return newfloatobject(v->ob_fval * ((floatobject *)w)->ob_fval);
 }
 
 static object *
-float_div(v, w)
-	floatobject *v;
-	object *w;
+float_div(floatobject *v, object *w)
 {
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	if (((floatobject *)w) -> ob_fval == 0) {
+	if (((floatobject *)w)->ob_fval == 0) {
 		err_setstr(ZeroDivisionError, "float division by zero");
 		return NULL;
 	}
-	return newfloatobject(v->ob_fval / ((floatobject *)w) -> ob_fval);
+	return newfloatobject(v->ob_fval / ((floatobject *)w)->ob_fval);
 }
 
 static object *
-float_rem(v, w)
-	floatobject *v;
-	object *w;
+float_rem(floatobject *v, object *w)
 {
 	double wx;
+
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
-	wx = ((floatobject *)w) -> ob_fval;
+	wx = ((floatobject *)w)->ob_fval;
 	if (wx == 0.0) {
 		err_setstr(ZeroDivisionError, "float division by zero");
 		return NULL;
@@ -181,19 +171,19 @@ float_rem(v, w)
 }
 
 static object *
-float_pow(v, w)
-	floatobject *v;
-	object *w;
+float_pow(floatobject *v, object *w)
 {
 	double iv, iw, ix;
+
 	if (!is_floatobject(w)) {
 		err_badarg();
 		return NULL;
 	}
 	iv = v->ob_fval;
 	iw = ((floatobject *)w)->ob_fval;
-	if (iw == 0.0)
+	if (iw == 0.0) {
 		return newfloatobject(1.0); /* x**0 is always 1, even 0**0 */
+    }
 	errno = 0;
 	ix = pow(iv, iw);
 	if (errno != 0) {
@@ -205,28 +195,26 @@ float_pow(v, w)
 }
 
 static object *
-float_neg(v)
-	floatobject *v;
+float_neg(floatobject *v)
 {
 	return newfloatobject(-v->ob_fval);
 }
 
 static object *
-float_pos(v)
-	floatobject *v;
+float_pos(floatobject *v)
 {
 	return newfloatobject(v->ob_fval);
 }
 
 static number_methods float_as_number = {
-	float_add,	/*tp_add*/
-	float_sub,	/*tp_subtract*/
-	float_mul,	/*tp_multiply*/
-	float_div,	/*tp_divide*/
-	float_rem,	/*tp_remainder*/
-	float_pow,	/*tp_power*/
-	float_neg,	/*tp_negate*/
-	float_pos,	/*tp_plus*/
+	(binaryfunc)float_add,	/*tp_add*/
+	(binaryfunc)float_sub,	/*tp_subtract*/
+	(binaryfunc)float_mul,	/*tp_multiply*/
+	(binaryfunc)float_div,	/*tp_divide*/
+	(binaryfunc)float_rem,	/*tp_remainder*/
+	(ternaryfunc)float_pow,	/*tp_power*/
+	(unaryfunc)float_neg,	/*tp_negate*/
+	(unaryfunc)float_pos,	/*tp_plus*/
 };
 
 typeobject Floattype = {
@@ -235,15 +223,15 @@ typeobject Floattype = {
 	"float",
 	sizeof(floatobject),
 	0,
-	float_dealloc,			/*tp_dealloc*/
-	float_print,		/*tp_print*/
-	0,			/*tp_getattr*/
-	0,			/*tp_setattr*/
-	float_compare,		/*tp_compare*/
-	float_repr,		/*tp_repr*/
-	&float_as_number,	/*tp_as_number*/
-	0,			/*tp_as_sequence*/
-	0,			/*tp_as_mapping*/
+	(destructor)float_dealloc,	/*tp_dealloc*/
+	(printfunc)float_print,		/*tp_print*/
+	0,							/*tp_getattr*/
+	0,							/*tp_setattr*/
+	(cmpfunc)float_compare,		/*tp_compare*/
+	(reprfunc)float_repr,		/*tp_repr*/
+	&float_as_number,			/*tp_as_number*/
+	0,							/*tp_as_sequence*/
+	0,							/*tp_as_mapping*/
 };
 
 /*
