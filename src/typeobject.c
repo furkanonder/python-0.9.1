c569f45e06c2bfc9f5e2683f948ce1d8964eddaa
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 17:08:50 +0300

    Convert K&R style function declarations to ANSI C style in typeobject.c

--- src/typeobject.c
+++ src/typeobject.c
@@ -2,36 +2,31 @@
 
 #include "allobjects.h"
 
-/* Type object implementation */
-
 static void
-type_print(v, fp, flags)
-	typeobject *v;
-	FILE *fp;
-	int flags;
+type_print(typeobject *v, FILE *fp, int flags)
 {
 	fprintf(fp, "<type '%s'>", v->tp_name);
 }
 
 static object *
-type_repr(v)
-	typeobject *v;
+type_repr(typeobject *v)
 {
 	char buf[100];
+
 	sprintf(buf, "<type '%.80s'>", v->tp_name);
 	return newstringobject(buf);
 }
 
 typeobject Typetype = {
 	OB_HEAD_INIT(&Typetype)
-	0,			/* Number of items for varobject */
-	"type",			/* Name of this type */
-	sizeof(typeobject),	/* Basic object size */
-	0,			/* Item size for varobject */
-	0,			/*tp_dealloc*/
-	type_print,		/*tp_print*/
-	0,			/*tp_getattr*/
-	0,			/*tp_setattr*/
-	0,			/*tp_compare*/
-	type_repr,		/*tp_repr*/
+	0,							/* Number of items for varobject */
+	"type",						/* Name of this type */
+	sizeof(typeobject),			/* Basic object size */
+	0,							/* Item size for varobject */
+	0,							/*tp_dealloc*/
+	(printfunc)type_print,		/*tp_print*/
+	0,							/*tp_getattr*/
+	0,							/*tp_setattr*/
+	0,							/*tp_compare*/
+	(reprfunc)type_repr,		/*tp_repr*/
 };
