e8957ad32bb0f42eeba30e27bd416a4fde4b83a9
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 13:36:13 +0300

    Convert K&R style function declarations to ANSI C style in strerror.c

--- src/strerror.c
+++ src/strerror.c
@@ -4,12 +4,13 @@ extern int sys_nerr;
 extern char *sys_errlist[];
 
 char *
-strerror(err)
-	int err;
+strerror(int err)
 {
 	static char buf[20];
-	if (err >= 0 && err < sys_nerr)
+
+	if (err >= 0 && err < sys_nerr) {
 		return sys_errlist[err];
+    }
 	sprintf(buf, "Unknown errno %d", err);
 	return buf;
 }
