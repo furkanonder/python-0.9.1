c49ffbd47afe63accc51424df7cde208f6a60b64
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 03:12:41 +0300

    Convert K&R style function declarations to ANSI C style in methodobject.c

--- src/methodobject.c
+++ src/methodobject.c
@@ -1,7 +1,6 @@
 /* Method object implementation */
 
 #include "allobjects.h"
-
 #include "token.h"
 
 typedef struct {
@@ -11,80 +10,78 @@ typedef struct {
 	object *m_self;
 } methodobject;
 
+/* char *name: static string */
 object *
-newmethodobject(name, meth, self)
-	char *name; /* static string */
-	method meth;
-	object *self;
+newmethodobject(char *name, method meth, object *self)
 {
 	methodobject *op = NEWOBJ(methodobject, &Methodtype);
+
 	if (op != NULL) {
 		op->m_name = name;
 		op->m_meth = meth;
-		if (self != NULL)
+		if (self != NULL) {
 			INCREF(self);
+        }
 		op->m_self = self;
 	}
 	return (object *)op;
 }
 
 method
-getmethod(op)
-	object *op;
+getmethod(object *op)
 {
 	if (!is_methodobject(op)) {
 		err_badcall();
 		return NULL;
 	}
-	return ((methodobject *)op) -> m_meth;
+	return ((methodobject *)op)->m_meth;
 }
 
 object *
-getself(op)
-	object *op;
+getself(object *op)
 {
 	if (!is_methodobject(op)) {
 		err_badcall();
 		return NULL;
 	}
-	return ((methodobject *)op) -> m_self;
+	return ((methodobject *)op)->m_self;
 }
 
 /* Methods (the standard built-in methods, that is) */
 
 static void
-meth_dealloc(m)
-	methodobject *m;
+meth_dealloc(methodobject *m)
 {
-	if (m->m_self != NULL)
+	if (m->m_self != NULL) {
 		DECREF(m->m_self);
+    }
 	free((char *)m);
 }
 
 static void
-meth_print(m, fp, flags)
-	methodobject *m;
-	FILE *fp;
-	int flags;
+meth_print(methodobject *m, FILE *fp, int flags)
 {
-	if (m->m_self == NULL)
+	if (m->m_self == NULL) {
 		fprintf(fp, "<built-in function '%s'>", m->m_name);
-	else
+    }
+	else {
 		fprintf(fp, "<built-in method '%s' of some %s object>",
-			m->m_name, m->m_self->ob_type->tp_name);
+				m->m_name, m->m_self->ob_type->tp_name);
+    }
 }
 
 static object *
-meth_repr(m)
-	methodobject *m;
+meth_repr(methodobject *m)
 {
 	char buf[200];
-	if (m->m_self == NULL)
+
+	if (m->m_self == NULL) {
 		sprintf(buf, "<built-in function '%.80s'>", m->m_name);
-	else
-		sprintf(buf,
-			"<built-in method '%.80s' of some %.80s object>",
-			m->m_name, m->m_self->ob_type->tp_name);
+    }
+	else {
+		sprintf(buf, "<built-in method '%.80s' of some %.80s object>",
+				m->m_name, m->m_self->ob_type->tp_name);
+    }
 	return newstringobject(buf);
 }
 
@@ -94,15 +91,15 @@ typeobject Methodtype = {
 	"method",
 	sizeof(methodobject),
 	0,
-	meth_dealloc,	/*tp_dealloc*/
-	meth_print,	/*tp_print*/
-	0,		/*tp_getattr*/
-	0,		/*tp_setattr*/
-	0,		/*tp_compare*/
-	meth_repr,	/*tp_repr*/
-	0,		/*tp_as_number*/
-	0,		/*tp_as_sequence*/
-	0,		/*tp_as_mapping*/
+	(destructor)meth_dealloc,	/*tp_dealloc*/
+	(printfunc)meth_print,		/*tp_print*/
+	0,							/*tp_getattr*/
+	0,							/*tp_setattr*/
+	0,							/*tp_compare*/
+	(reprfunc)meth_repr,		/*tp_repr*/
+	0,							/*tp_as_number*/
+	0,							/*tp_as_sequence*/
+	0,							/*tp_as_mapping*/
 };
 
 /* Find a method in a module's method table.
