1a64f38ab4f6731cf37d4b5eee75fdc681c44714
Furkan Onder <furkanonder@protonmail.com>
2024-11-02 03:50:54 +0300

    Convert K&R style function declarations to ANSI C style in object.c

--- src/object.c
+++ src/object.c
@@ -12,12 +12,13 @@ long ref_total;
    Do not call them otherwise, they do not initialize the object! */
 
 object *
-newobject(tp)
-	typeobject *tp;
+newobject(typeobject *tp)
 {
-	object *op = (object *) malloc(tp->tp_basicsize);
-	if (op == NULL)
+	object *op = (object *)malloc(tp->tp_basicsize);
+
+	if (op == NULL) {
 		return err_nomem();
+    }
 	NEWREF(op);
 	op->ob_type = tp;
 	return op;
@@ -26,14 +27,13 @@ newobject(tp)
 #if 0 /* unused */
 
 varobject *
-newvarobject(tp, size)
-	typeobject *tp;
-	unsigned int size;
+newvarobject(typeobject *tp, unsigned int size)
 {
-	varobject *op = (varobject *)
-		malloc(tp->tp_basicsize + size * tp->tp_itemsize);
-	if (op == NULL)
+	varobject *op = (varobject *)malloc(tp->tp_basicsize
+ 					 + size * tp->tp_itemsize);
+	if (op == NULL) {
 		return err_nomem();
+	}
 	NEWREF(op);
 	op->ob_type = tp;
 	op->ob_size = size;
@@ -43,14 +43,10 @@ newvarobject(tp, size)
 #endif
 
 int StopPrint; /* Flag to indicate printing must be stopped */
-
 static int prlevel;
 
 void
-printobject(op, fp, flags)
-	object *op;
-	FILE *fp;
-	int flags;
+printobject(object *op, FILE *fp, int flags)
 {
 	/* Hacks to make printing a long or recursive object interruptible */
 	/* XXX Interrupts should leave a more permanent error */
@@ -64,11 +60,12 @@ printobject(op, fp, flags)
 			fprintf(fp, "<nil>");
 		}
 		else {
-			if (op->ob_refcnt <= 0)
+			if (op->ob_refcnt <= 0) {
 				fprintf(fp, "(refcnt %d):", op->ob_refcnt);
+            }
 			if (op->ob_type->tp_print == NULL) {
 				fprintf(fp, "<%s object at %lx>",
-					op->ob_type->tp_name, (long)op);
+                        op->ob_type->tp_name, (long)op);
 			}
 			else {
 				(*op->ob_type->tp_print)(op, fp, flags);
@@ -76,17 +73,18 @@ printobject(op, fp, flags)
 		}
 	}
 	prlevel--;
-	if (prlevel == 0)
+	if (prlevel == 0) {
 		StopPrint = 0;
+    }
 }
 
 object *
-reprobject(v)
-	object *v;
+reprobject(object *v)
 {
 	object *w = NULL;
 	/* Hacks to make converting a long or recursive object interruptible */
 	prlevel++;
+
 	if (!StopPrint && intrcheck()) {
 		StopPrint = 1;
 		err_set(KeyboardInterrupt);
@@ -98,7 +96,7 @@ reprobject(v)
 		else if (v->ob_type->tp_repr == NULL) {
 			char buf[100];
 			sprintf(buf, "<%.80s object at %lx>",
-				v->ob_type->tp_name, (long)v);
+                    v->ob_type->tp_name, (long)v);
 			w = newstringobject(buf);
 		}
 		else {
@@ -110,33 +108,37 @@ reprobject(v)
 		}
 	}
 	prlevel--;
-	if (prlevel == 0)
+	if (prlevel == 0) {
 		StopPrint = 0;
+    }
 	return w;
 }
 
 int
-cmpobject(v, w)
-	object *v, *w;
+cmpobject(object *v, object *w)
 {
 	typeobject *tp;
-	if (v == w)
+
+	if (v == w) {
 		return 0;
-	if (v == NULL)
+    }
+	if (v == NULL) {
 		return -1;
-	if (w == NULL)
+    }
+	if (w == NULL) {
 		return 1;
-	if ((tp = v->ob_type) != w->ob_type)
+    }
+	if ((tp = v->ob_type) != w->ob_type) {
 		return strcmp(tp->tp_name, w->ob_type->tp_name);
-	if (tp->tp_compare == NULL)
+    }
+	if (tp->tp_compare == NULL) {
 		return (v < w) ? -1 : 1;
+    }
 	return ((*tp->tp_compare)(v, w));
 }
 
 object *
-getattr(v, name)
-	object *v;
-	char *name;
+getattr(object *v, char *name)
 {
 	if (v->ob_type->tp_getattr == NULL) {
 		err_setstr(TypeError, "attribute-less object");
@@ -148,16 +150,15 @@ getattr(v, name)
 }
 
 int
-setattr(v, name, w)
-	object *v;
-	char *name;
-	object *w;
+setattr(object *v, char *name, object *w)
 {
 	if (v->ob_type->tp_setattr == NULL) {
-		if (v->ob_type->tp_getattr == NULL)
+		if (v->ob_type->tp_getattr == NULL) {
 			err_setstr(TypeError, "attribute-less object");
-		else
+        }
+		else {
 			err_setstr(TypeError, "object has read-only attributes");
+        }
 		return -1;
 	}
 	else {
@@ -171,19 +172,14 @@ NoObject is usable as a non-NULL undefined value, used by the macro None.
 There is (and should be!) no way to create other objects of this type,
 so there is exactly one (which is indestructible, by the way).
 */
-
 static void
-none_print(op, fp, flags)
-	object *op;
-	FILE *fp;
-	int flags;
+none_print(object *op, FILE *fp, int flags)
 {
 	fprintf(fp, "None");
 }
 
 static object *
-none_repr(op)
-	object *op;
+none_repr(object *op)
 {
 	return newstringobject("None");
 }
@@ -194,15 +190,15 @@ static typeobject Notype = {
 	"None",
 	0,
 	0,
-	0,		/*tp_dealloc*/ /*never called*/
-	none_print,	/*tp_print*/
-	0,		/*tp_getattr*/
-	0,		/*tp_setattr*/
-	0,		/*tp_compare*/
-	none_repr,	/*tp_repr*/
-	0,		/*tp_as_number*/
-	0,		/*tp_as_sequence*/
-	0,		/*tp_as_mapping*/
+	0,						/*tp_dealloc*/ /*never called*/
+	(printfunc)none_print,	/*tp_print*/
+	0,						/*tp_getattr*/
+	0,						/*tp_setattr*/
+	0,						/*tp_compare*/
+	(reprfunc)none_repr,	/*tp_repr*/
+	0,						/*tp_as_number*/
+	0,						/*tp_as_sequence*/
+	0,						/*tp_as_mapping*/
 };
 
 object NoObject = {
@@ -214,8 +210,7 @@ object NoObject = {
 
 static object refchain = {&refchain, &refchain};
 
-NEWREF(op)
-	object *op;
+NEWREF(object *op)
 {
 	ref_total++;
 	op->ob_refcnt = 1;
@@ -225,17 +220,18 @@ NEWREF(op)
 	refchain._ob_next = op;
 }
 
-UNREF(op)
-	register object *op;
+UNREF(register object *op)
 {
 	register object *p;
+
 	if (op->ob_refcnt < 0) {
 		fprintf(stderr, "UNREF negative refcnt\n");
 		abort();
 	}
 	for (p = refchain._ob_next; p != &refchain; p = p->_ob_next) {
-		if (p == op)
+		if (p == op) {
 			break;
+        }
 	}
 	if (p == &refchain) { /* Not found */
 		fprintf(stderr, "UNREF unknown object\n");
@@ -245,17 +241,16 @@ UNREF(op)
 	op->_ob_prev->_ob_next = op->_ob_next;
 }
 
-DELREF(op)
-	object *op;
+DELREF(object *op)
 {
 	UNREF(op);
 	(*(op)->ob_type->tp_dealloc)(op);
 }
 
-printrefs(fp)
-	FILE *fp;
+printrefs(FILE *fp)
 {
 	object *op;
+
 	fprintf(fp, "Remaining objects:\n");
 	for (op = refchain._ob_next; op != &refchain; op = op->_ob_next) {
 		fprintf(fp, "[%d] ", op->ob_refcnt);
