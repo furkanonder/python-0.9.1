8d4b1a78adf19a8560deb8923a33779bcd11c892
Furkan Onder <furkanonder@protonmail.com>
2024-11-03 22:46:19 +0300

    Remove redundant preprocessor directives in sigtype.h

--- src/sigtype.h
+++ src/sigtype.h
@@ -1,15 +1,5 @@
 /* The type of signal handlers is somewhat problematic.
    This file encapsulates my knowledge about it:
-   - on the Mac (THINK C), it's int for 3.0, void for 4.0
-   - on other systems, it's usually void, except it's int on vax Ultrix.
-   Pass -DSIGTYPE=... to cc if you know better. */
+   It's usually void. Pass -DSIGTYPE=... to cc if you know better. */
 
-#ifndef SIGTYPE
-
-#if defined(vax) && !defined(AMOEBA)
-#define SIGTYPE int
-#else
 #define SIGTYPE void
-#endif
-
-#endif /* !SIGTYPE */
